fbe9df8a6ab1932dfbe2f8b1664a1980ee940eb3
aldacron <aldacron@gmail.com>
2016-02-01 11:18:02 +0900

    Add error code from postgres_ext.h

--- source/derelict/pq/pq.d
+++ source/derelict/pq/pq.d
@@ -197,6 +197,27 @@ extern(C) {
         PGresult* result;
     }
 
+    // from postgres_ext.h
+    enum : int {
+        PG_DIAG_SEVERITY =          'S',
+        PG_DIAG_SQLSTATE =          'C',
+        PG_DIAG_MESSAGE_PRIMARY =   'M',
+        PG_DIAG_MESSAGE_DETAIL =    'D',
+        PG_DIAG_MESSAGE_HINT =      'H',
+        PG_DIAG_STATEMENT_POSITION ='P',
+        PG_DIAG_INTERNAL_POSITION = 'p',
+        PG_DIAG_INTERNAL_QUERY =    'q',
+        PG_DIAG_CONTEXT =           'W',
+        PG_DIAG_SCHEMA_NAME =       's',
+        PG_DIAG_TABLE_NAME =        't',
+        PG_DIAG_COLUMN_NAME =       'c',
+        PG_DIAG_DATATYPE_NAME =     'd',
+        PG_DIAG_CONSTRAINT_NAME =   'n',
+        PG_DIAG_SOURCE_FILE =       'F',
+        PG_DIAG_SOURCE_LINE =       'L',
+        PG_DIAG_SOURCE_FUNCTION =   'R'
+    }
+
     extern( C ) nothrow {
         alias PQnoticeReceiver = void function( void*,PGresult* );
         alias PQnoticeProcessor = void function( void*,char* );
